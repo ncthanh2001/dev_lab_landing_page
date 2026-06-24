766e2037ed13f83b2abbced0caa2e2bfa6caae70
thanhnc <thanhnc@digitalbiz.com.vn>
2026-06-24 22:49:54 +0700

    init project

---
+++ src/config.ts
@@ -0,0 +1,3 @@
+// Thông tin liên hệ dùng chung cho toàn site.
+// 👉 Khi có Zalo OA / số Zalo thật của DevLab, chỉ cần sửa ở đây là mọi nút "Liên hệ" cập nhật theo.
+export const ZALO_URL = "https://zalo.me/0900123456"; // TODO: thay bằng link Zalo thật
